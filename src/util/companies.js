81090465861baa3621052b3611dfd4bd00fdefcf
Isaakavo <isaakhaas96@gmail.com>
2020-11-13 14:26:05 -0600

    File created

---
+++ src/util/companies.js
@@ -0,0 +1,9 @@
+export const companies = [
+  { value: 'impacto', label: 'Impacto Biomédico' },
+  { value: 'material sin empresa', label: 'Material Sin Empresa' },
+  { value: 'ya mas', label: 'Ya Más' },
+  { value: 'espectro', label: 'Espectro Biomédico' },
+  { value: 'tainos', label: 'Tainos' },
+  { value: 'material usado', label: 'Material Usado' },
+  { value: 'resguardos', label: 'Resguardos' },
+];
