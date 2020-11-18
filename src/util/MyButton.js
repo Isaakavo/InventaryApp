492e1c912e61e7c55bbe5b5fc288dfd8d2bdad38
Isaakavo <isaakhaas96@gmail.com>
2020-11-18 16:30:02 -0600

    Added disabled option

--- src/util/MyButton.js
+++ src/util/MyButton.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
-const MyButton = ({ children, onClick, tip, btnClassName, tipClassName }) => {
+const MyButton = ({
+  children,
+  onClick,
+  tip,
+  btnClassName,
+  tipClassName,
+  disabled,
+}) => {
   return (
     <Tooltip title={tip} className={tipClassName}>
-      <IconButton onClick={onClick} className={btnClassName}>
-        {children}
-      </IconButton>
+      <div>
+        <IconButton
+          onClick={onClick}
+          className={btnClassName}
+          disabled={disabled}
+        >
+          {children}
+        </IconButton>
+      </div>
     </Tooltip>
   );
 };
