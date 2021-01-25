af2aba9fdc7cc77967410c68c452a8a38c6c1f2f
Isaakavo <isaakhaas96@gmail.com>
2021-01-25 16:30:49 -0600

    Now you can delete items

--- src/redux/types.js
+++ src/redux/types.js
@@ -7,10 +7,12 @@ export const STOP_LOADING_DATA = 'STOP_LOADING_DATA';
 export const STOP_LOADING_UI = 'STOP_LOADING_UI';
 //DATA reducer types
 export const SET_DATA = 'SET_DATA';
+export const DELETE_DATA = 'DELETE_DATA';
 export const ITEM_ADDED = 'ITEM_ADDED';
 export const CHANGE_DATABASE = 'CHANGE_DATABASE';
 export const SET_LASTNUM = 'SET_LASTNUM';
 export const SET_ALL_DATA = 'SET_ALL_DATA';
+export const UPDATE_DATA = 'UPDATE_DATA';
 //USER reducer types
 export const SET_AUTHENTICATED = 'SET_AUTHENTICATED';
 export const SET_UNAUTHENTICATED = 'SET_UNAUTHENTICATED';
